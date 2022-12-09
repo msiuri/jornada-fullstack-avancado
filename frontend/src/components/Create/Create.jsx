218e13c617df0ca7a744bdebd42c9327b95ad44c
Iuri Moura <msiuri@outlook.com>
2022-12-08 21:55:42 -0300

    Frontend com select de category

--- frontend/src/components/Create/Create.jsx
+++ frontend/src/components/Create/Create.jsx
@@ -1,20 +1,42 @@
 import { useNavigate } from "react-router-dom";
 import { Api } from "../../api/api";
 import "./Create.css";
+import Select from "react-select";
+import { useState } from "react";
+import { useEffect } from "react";
+import { faUnderline } from "@fortawesome/free-solid-svg-icons";
 
 function Create() {
   const navigate = useNavigate();
+
+  const [categories, setCategories] = useState();
+
+  console.log(categories);
+
+  async function loadCategories() {
+    console.log("Carregar categorias");
+    const readAll = Api.category.readAll();
+
+    const response = await Api.buildApiGetRequest(readAll);
+
+    const resultado = await response.json();
+    setCategories(resultado);
+  }
+  useEffect(function () {
+    loadCategories();
+  }, []);
 
   async function processarSubmit(event) {
     event.preventDefault();
 
     const name = event.target.nome.value;
     const imageUrl = event.target.imageUrl.value;
+    const category = event.target.category.value;
 
     const payload = {
       name,
       imageUrl,
-      category: "639136e2f875bd00698cf3f6",
+      category,
     };
 
     const createUrl = Api.item.create();
@@ -33,13 +55,23 @@ function Create() {
     <div className="Create">
       <form onSubmit={processarSubmit}>
         <div>
-          <label htmlFor="nome">Nome:</label>
+          <label htmlFor="nome">Nome*:</label>
           <input type="text" id="nome" />
         </div>
 
         <div>
           <label htmlFor="imageUrl">URL da Imagem:</label>
           <input type="text" id="imageUrl" />
+        </div>
+
+        <div>
+          <label htmlFor="category">Categoria*:</label>
+          <Select
+            options={categories?.map(function (category) {
+              return { value: category._id, label: category.name };
+            })}
+            name="category"
+          />
         </div>
 
         <div>
